e4fe6df019cf9dbe5626c05ec6aba75436d886f4
Nigel Shen <86505891+Nigel-Shen@users.noreply.github.com>
2022-05-25 10:03:53 -0400

    Add files via upload

--- manufactured.py
+++ manufactured.py
@@ -5,45 +5,18 @@ from scipy import sparse
 import scipy.sparse.linalg
 from mpmath import findroot, erfc
 import matplotlib.pyplot as plt
+from toolbox import *
 
 
-def phaseSpace(xp, vp, wp, L, pixel=200):
-    X = L
-    V = 10 * (1 + 1/pixel)
-    M = np.zeros([pixel, pixel])
-    x = X / pixel
-    v = 2 * V / pixel
-    for i in range(len(xp)):
-        if np.abs(vp[i]) < 10:
-            M[int((vp[i] + V) // v), int(xp[i] // x) % pixel] = M[int((vp[i] + V) // v), int(xp[i] // x) % pixel] + wp[i]
-    plt.imshow(M, vmin=0, vmax=np.max(M), cmap='plasma', interpolation="nearest", extent=[0, X, -V, V])
-    plt.colorbar()
+def findsource(xp, vp, L, it, DT):
+    E = Q * N * np.sin(np.pi * it * DT / 4) * np.cos(2 * np.pi * xp / L) / (4 * np.pi)
+    S1 = - 0.5 * (np.sqrt(np.pi / 2) / L) * np.exp(-vp ** 2 / 2) * np.cos(np.pi * it * DT / 4) * np.sin(2 * np.pi * xp / L) / 4
+    S2 = - 0.5 * (np.sqrt(2 * np.pi) * vp / L ** 2) * np.exp(- vp ** 2 / 2) * np.sin(np.pi * it * DT / 4) * np.cos(
+        2 * np.pi * xp / L)
+    S3 = - (QM * E / L) * (1 - 0.5 * np.sin(np.pi * it * DT / 4) * np.sin(2 * np.pi * xp / L)) * (np.exp(- vp ** 2 / 2) * vp / np.sqrt(2 * np.pi))
+    return S1 + S2 + S3
 
-
-def showDistribution(xp, L, resolution):
-    x = np.linspace(0, L, resolution + 1)
-    y = np.zeros(resolution)
-    length = L / resolution
-    for item in xp:
-        y[int(item // length)] = y[int(item // length)] + 1
-    plt.plot(x[0:resolution], y)  # Phase space at a given time
-    plt.show()
-
-
-def showChargeDensity(xp, Q, dx, NG, L, rho_back, resolution):
-    x = np.linspace(0, L, resolution)
-    y = np.ones(resolution) * rho_back * L / resolution
-    for i in range(resolution):
-        for item in xp:
-            if item - 0.5 * dx < x[i] < item + 0.5 * dx:
-                y[i] = y[i] + Q * NG / resolution
-            elif x[i] < item - L + 0.5 * dx or x[i] > item + L - 0.5 * dx:
-                y[i] = y[i] + Q * NG / resolution
-    plt.plot(x, y)  # Phase space at a given time
-    plt.show()
-
-
-L = 10 * np.pi  # Length of the container
+L = 2 * np.pi  # Length of the container
 DT = .005  # Length of a time step
 NT = 1600  # number of time steps
 NG = 128  # Number of Grid points
@@ -61,14 +34,14 @@ k = lambdaD * mode * 2 * np.pi / L
 
 i = 0
 xp = np.random.rand(N) * L
-vp = np.zeros(N)
-while i < N:
-    v = np.random.rand(1, 2) * np.array([8, 1]) - np.array([4,0])
-    if v[0, 1] < 2 * v[0, 0] ** 2 * np.exp(- v[0, 0] ** 2):
-        vp[i] = v[0, 0]
-        i = i + 1
+vp = np.random.randn(N)
+# while i < N:
+#     v = np.random.rand(1, 2) * np.array([8, 1]) - np.array([4, 0])
+#     if v[0, 1] < 2 * v[0, 0] ** 2 * np.exp(- v[0, 0] ** 2):
+#         vp[i] = v[0, 0]
+#         i = i + 1
 wp = np.ones(N)
-f0 = 2 * vp ** 2 * np.exp(- vp ** 2) / (L * np.sqrt(np.pi))
+f0 = np.exp(- vp ** 2 / 2) / (L * np.sqrt(2 * np.pi))
 # Perturbation
 p = np.linspace(0, N - 1, N).astype(int)
 un = np.ones(NG - 1)
@@ -80,8 +53,7 @@ Ep = []  # Potential Energy
 Et = []  # Total Energy
 momentum = []
 PhiMax = []
-# showDistribution(xp, Q, NG, L, 64)
-picnum=0
+picnum = 0
 plt.rcParams['figure.dpi'] = 300
 for it in range(NT):
     print(it)
@@ -89,14 +61,29 @@ for it in range(NT):
         picnum = picnum + 1
         plt.subplot(4, 4, picnum)
         phaseSpace(xp, vp, wp, L)
-        plt.title('$t$=%s' % str(np.round(it * DT,4)))
+        plt.title('$t$=%s' % str(np.round(it * DT, 4)))
 
-    xp = xp + vp * DT / 2
     # Apply bc on the particle position, periodic
-    out = (xp < 0)
-    xp[out] = xp[out] + L
-    out = (xp >= L)
-    xp[out] = xp[out] - L
+    if it == 0:
+        g1 = np.floor(xp / dx - 0.5).astype(int)  # which grid point to project onto
+        g = np.array([g1, g1 + 1])  # used to determine bc
+        fraz1 = (1 - abs(xp / dx - g1 - 0.5)) * wp
+        fraz = np.array([fraz1, wp - fraz1])
+
+        # apply bc on the projection
+        g = toPeriodic(g, NG, True)
+        mat = sparse.csr_matrix((fraz[0], (p, g[0]))) + sparse.csr_matrix((fraz[1], (p, g[1])))  # interpolation
+        rho = np.asarray((Q / dx) * mat.sum(0) + rho_back * np.ones([1, NG]))
+
+        # computing fields
+        Phi = sparse.linalg.splu(Poisson).solve(-rho[0, 0:NG - 1] * dx ** 2)
+        Phi = np.append(Phi, [0])
+        Eg = np.transpose([np.append(Phi[NG - 1], Phi[0:NG - 1]) - np.append(Phi[1:NG], Phi[0])]) / (2 * dx)
+        # projection p -> q and update of vp
+        vp = vp + np.transpose(mat * Eg)[0] * QM * DT / 2
+        wp = wp + DT * findsource(xp + vp * DT / 2, vp, L, 0.5, DT) / f0
+
+    xp = toPeriodic(xp,L)
 
     # projection p->g
     g1 = np.floor(xp / dx - 0.5).astype(int)  # which grid point to project onto
@@ -105,10 +92,7 @@ for it in range(NT):
     fraz = np.array([fraz1, wp - fraz1])
 
     # apply bc on the projection
-    out = (g < 0)
-    g[out] = g[out] + NG
-    out = (g > NG - 1)
-    g[out] = g[out] - NG
+    g = toPeriodic(g, NG, True)
     mat = sparse.csr_matrix((fraz[0], (p, g[0]))) + sparse.csr_matrix((fraz[1], (p, g[1])))  # interpolation
     rho = np.asarray((Q / dx) * mat.sum(0) + rho_back * np.ones([1, NG]))
 
@@ -118,15 +102,9 @@ for it in range(NT):
     Eg = np.transpose([np.append(Phi[NG - 1], Phi[0:NG - 1]) - np.append(Phi[1:NG], Phi[0])]) / (2 * dx)
     # projection p -> q and update of vp
 
-    if it == 0: # leap frog
-        vp = vp + np.transpose(mat * Eg)[0] * QM * DT / 2
-    xp = xp + vp * DT / 2
+    xp = xp + vp * DT
     vp = vp + np.transpose(mat * Eg)[0] * QM * DT
-    E = - Q * np.sin(np.pi * it * DT) * np.cos(2 * np.pi * xp / L) / (2 * np.pi)
-    S1 = - (2 * np.sqrt(np.pi) / L) * vp ** 2 * np.exp(-vp ** 2) * np.cos(np.pi * it * DT) * np.sin(2 * np.pi * xp / L)
-    S2 = - (4 * np.sqrt(np.pi) * vp ** 3 / L ** 2) * np.exp(- vp ** 2) * np.sin(np.pi * it * DT) * np.cos(2 * np.pi * xp / L)
-    S3 = - (QM * E / L) * (1 - np.sin(np.pi * it * DT) * np.sin(2 * np.pi * xp / L)) * (4 * np.exp(- vp ** 2) / np.sqrt(np.pi)) * (vp - vp ** 3)
-    wp = wp + DT * (S1 + S2 + S3) / f0
+    wp = wp + DT * findsource(xp, vp, L, it, DT) / f0
     # energies
     kinetic = sum(Q * wp * vp * vp * 0.5 / QM)
     potential = sum(mat * Phi * Q * wp)
