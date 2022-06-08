c35371e4890646ea67258cac7f85ac75317ad533
Nigel Shen <86505891+Nigel-Shen@users.noreply.github.com>
2022-06-08 16:27:59 -0400

    Update trueDistribution.py

--- trueDistribution.py
+++ trueDistribution.py
@@ -2,23 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def trueDistribution(t):
-    image = np.zeros([128, 128])
-    for i in range(128):
-        for j in range(128):
-            image[j,i] = np.exp(-((64-j)/6.4)**2/2)*(1-0.5*np.sin(np.pi * t/4)*np.sin(2*np.pi*i/128))/(128*np.sqrt(np.pi*2))
+def trueDistribution(t, NG):
+    image = np.zeros([NG, NG])
+    for i in range(NG):
+        for j in range(NG):
+            image[j,i] = np.exp(-((NG/2-(j+0.5))/(NG/20))**2/2)*(1-0.5*np.sin(np.pi * t/4)*np.sin(2*np.pi*(i+0.5)/NG))/(NG*np.sqrt(np.pi*2))
     plt.imshow(image, 'plasma', interpolation='nearest')
 
+def trueRho(t, NG, N, Q, L):
+    y = np.linspace(0, 1, NG, endpoint=False) + 1 / (2 * NG)
+    return N * Q * (1 - 0.5 * np.sin(np.pi * t / 4) * np.sin(2*np.pi*y)) / L
 
+def trueField(t, NG):
+    y=np.zeros(NG)
+    for i in range(NG):
+        y[i] = np.sin(np.pi * t/4)*np.cos(2*np.pi*(i+0.5)/NG)
+    return y
 
-def trueField(t):
-    y=np.zeros(128)
-    for i in range(128):
-        y[i] = np.sin(np.pi * t/4)*np.cos(2*np.pi*i/128)
-    plt.plot(np.linspace(0,127,128), y)
-    plt.ylim([-1.2,1.2])
-
-for t in [0,1,2,3,4,5,6,7]:
-    trueField(t)
-    plt.savefig('E%s.png' %t)
-    plt.clf()
+def truePhi(t, NG, N, Q, L):
+    y = np.linspace(0, 1, NG, endpoint=False) + 1 / (2 * NG)
+    return N * Q * L * (1 - 0.5 * np.sin(np.pi * t / 4) * np.sin(2 * np.pi * y)) / (8 * np.pi ** 2)
