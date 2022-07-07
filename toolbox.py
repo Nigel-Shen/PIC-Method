ba59473d5160725e7ea203cdb8eac0dacd1b1392
Nigel Shen <86505891+Nigel-Shen@users.noreply.github.com>
2022-07-07 19:34:41 -0400

    Update toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -6,25 +6,26 @@ import scipy.sparse.linalg
 from mpmath import findroot, erfc
 import matplotlib.pyplot as plt
 
+
 def toPeriodic(x, L, discrete=False):
     out = (x < 0)
-    x[out] = x[out] % L
+    x[out] = x[out] + L
     if discrete:
         out = (x > L - 1)
     else:
         out = (x >= L)
-    x[out] = x[out] % L
+    x[out] = x[out] - L
     return x
 
 
-def phaseSpace(g, fraz, vp, Q):
-    g = g[:, np.abs(vp) < 10]
-    fraz = fraz[:, np.abs(vp) < 10]
-    vp = vp[np.abs(vp) < 10]
-    col = (vp + 10) // (20 / 128)
+def phaseSpace(g, fraz, vp, Q, VT):
+    g = g[:, np.abs(vp) < 10 * VT]
+    fraz = fraz[:, np.abs(vp) < 10 * VT]
+    vp = vp[np.abs(vp) < 10 * VT]
+    col = (vp + 10 * VT) // (20 *VT / 128)
     col = col.astype(int)
 
-    mat = sparse.csr_matrix((- fraz[0] * Q, (col, g[0]))) + sparse.csr_matrix((- fraz[1] * Q, (col, g[1])))
+    mat = sparse.csr_matrix((- fraz[0] * Q, (col, g[0]))) + sparse.csr_matrix((- fraz[1] * Q, (col, g[1]))) +sparse.csr_matrix((- fraz[2] * Q, (col, g[2])))
     mat = mat.todense()
     print(mat.ndim, np.zeros([128 - mat.shape[0], mat.shape[1]]).ndim)
     mat = np.append(mat, np.zeros([128 - mat.shape[0], mat.shape[1]]), axis=0)
@@ -43,19 +44,6 @@ def showDistribution(xp, L, resolution):
     plt.show()
 
 
-def showChargeDensity(xp, Q, dx, NG, L, rho_back, resolution):
-    x = np.linspace(0, L, resolution)
-    y = np.ones(resolution) * rho_back * L / resolution
-    for i in range(resolution):
-        for item in xp:
-            if item - 0.5 * dx < x[i] < item + 0.5 * dx:
-                y[i] = y[i] + Q * NG / resolution
-            elif x[i] < item - L + 0.5 * dx or x[i] > item + L - 0.5 * dx:
-                y[i] = y[i] + Q * NG / resolution
-    plt.plot(x, y)  # Phase space at a given time
-    plt.show()
-
-
 def findDecayRate(k):
     gamma = float(np.imag(findroot(
         lambda x: 1 + 1 / k ** 2 + 1j * x * cmath.exp(-x ** 2 / (2 * k ** 2)) * erfc(-1j * x / (math.sqrt(2) * k)) / (
@@ -63,8 +51,20 @@ def findDecayRate(k):
     return gamma
 
 
-def fieldSolve(rho, L):
-    rhoHat = np.fft.rfft(rho)
-    phiHat = np.append([0], rhoHat[1:] * (L / (2 * np.pi * np.arange(1, rhoHat.size))) ** 2)
-    phi = np.fft.irfft(phiHat)
-    return phi
+def fieldSolve(rho, L, hat=False):
+    if hat:
+        rhoHat = rho
+    else:
+        rhoHat = np.fft.fft(rho)     
+    Ka = np.arange(1, rhoHat.size // 2)
+    Kb = Ka[::-1]
+    K = np.append(np.append(Ka, [rhoHat.size // 2]), - Kb)
+    phiHat = np.append([0], rhoHat[1:] * (L / (2 * np.pi * K)) ** 2)
+    EHat = np.append([0], rhoHat[1:] * L / (2j * np.pi * K))
+    EHat[rhoHat.size // 2] = 0
+    if hat:
+        return phiHat, EHat
+    else:
+        phi = np.real(np.fft.ifft(phiHat))
+        E = np.real(np.fft.ifft(EHat))
+        return phi, E
