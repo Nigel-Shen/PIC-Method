7c25ec66bc8a4c710f6844748ad06ca7924f4ec1
Nigel Shen <86505891+Nigel-Shen@users.noreply.github.com>
2022-07-12 23:15:09 -0400

    Update PIF.py

--- PIF.py
+++ PIF.py
@@ -76,7 +76,7 @@ for it in range(NT):
     # energies
     kinetic = sum(Q * vp ** 2 * 0.5 / QM)
     potential1 = sum(rhoHat * np.conjugate(Phihat) / (2 * L))
-    Phi = np.fft.ifft(Phihat)
+    Phi = np.fft.ifft(Phihat) * NF / L
     Ek.append(kinetic)
     Ep.append(potential1)
     #Ep2.append(potential1)
